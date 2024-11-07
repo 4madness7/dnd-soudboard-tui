8b2eccae2b6072478870a2c202941fab8afa7497
4madness7 <marcomirizzi4@gmail.com>
2024-11-07 11:32:18 +0100

    feat: added import from directory

--- ui/input_file.py
+++ ui/input_file.py
@@ -1,9 +1,9 @@
 from textual.widgets import Input
 from textual.events import Hide
 import os
-import shutil
 
-from consts import FORMATS, MEDIA_PATH
+from consts import FORMATS
+from utils import copy_file
 
 class InputFile(Input):
 
@@ -14,24 +14,44 @@ class InputFile(Input):
         return super()._on_hide(event)
 
     def on_input_submitted(self):
-        self.value = self.value.strip()
-        is_file = os.path.isfile(self.value)
-        is_audio_file = self.value.split(".")[-1] in FORMATS
-        if is_file and is_audio_file:
-            filename = os.path.split(self.value)[-1]
-            full_path = os.path.join(MEDIA_PATH, filename)
-            if not os.path.exists(full_path):
-                shutil.copyfile(self.value, full_path,follow_symlinks=False)
+        input_path = self.value.strip()
+        self.add_class("none")
+
+        is_file = os.path.isfile(input_path)
+        is_dir = os.path.isdir(input_path)
+        is_audio_file = input_path.split(".")[-1] in FORMATS
+        if is_dir:
+            media_files = []
+            for f in os.listdir(input_path):
+                if os.path.isfile(os.path.join(input_path, f)) and f.split(".")[-1] in FORMATS:
+                    media_files.append(f)
+            media_files.sort()
+
+            count_saved = 0
+            for file_name in media_files:
+                copied, new_path = copy_file(os.path.join(input_path, file_name))
+                if copied:
+                    count_saved += 1
+                else:
+                    self.app.notify(f"File \"{new_path}\" already exists", severity="error")
+            self.app.notify(
+                f"Successfully copied {count_saved} audio files out of {len(media_files)}.",
+                severity="information"
+            )
+        elif is_file and is_audio_file:
+            copied, new_path = copy_file(input_path)
+            if copied:
                 self.app.notify(
-                        f"Successfully copied audio from \"{self.value}\" to \"{full_path}\"",
-                        severity="information"
-                    )
+                    f"Successfully copied audio from \"{input_path}\" to \"{new_path}\"",
+                    severity="information"
+                )
             else:
-                self.app.notify(f"File \"{full_path}\" already exists", severity="error")
+                self.app.notify(f"File \"{new_path}\" already exists", severity="error")
         else:
+            if not is_dir:
+                self.app.notify("Directory not found", severity="error")
             if not is_file:
                 self.app.notify("File not found", severity="error")
-            if not is_audio_file:
+            elif not is_audio_file:
                 self.app.notify(f"Not a valid format. Valid formats: {FORMATS}", severity="error")
-        self.add_class("none")
 
